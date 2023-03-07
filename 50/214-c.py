01fdf8ed94e415b67fcda7f5f62aea4ad6f5a24c
nanomi9ro <cattleya.leya0@gmail.com>
2023-03-07 23:44:18 +0900

    add: files

---
+++ 50/214-c.py
@@ -0,0 +1,14 @@
+n = int(input())
+s = list(map(int,input().split()))
+t = list(map(int,input().split()))
+
+time = [10**15]*n
+
+for i in range(n):
+    next=(i+1)%n
+
+time[next] = min(t[next],time[i]+s[i])
+
+for i in range(n):
+    print(time[i])
+    
